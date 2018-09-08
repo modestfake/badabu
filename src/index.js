f5ac40cdcfe9d4e100325767efdb5fdf4963219c
Vitalii Saienko <vsaienko@atlassian.com>
2018-09-08 19:30:12 +0300

    Update manifest.json, add app logo, enable service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset'
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
