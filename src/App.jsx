e730b7e225c81afe1c12fd14474cb67b5baeeb50
Vitalii Saienko <vsaienko@atlassian.com>
2018-09-09 13:58:24 +0300

    Replace logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Playlist from './components/Playlist'
 import './App.css';
+import logo from './logo.svg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src='badabu-logo-192.png' alt='Badabu logo' />
+          <img src={logo} alt='Badabu logo' />
           <h1 className="App-title">Badabu</h1>
         </header>
         <Playlist />
