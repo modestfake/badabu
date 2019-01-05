781f8ec8583ea3376beab3a1252e119bd7e3af4f
Vitalii Saienko <vsaienko@atlassian.com>
2019-01-06 01:11:46 +0200

    Move analytics to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ReactGA from 'react-ga'
 import Playlist from './components/Playlist'
-import './App.css';
+import './App.css'
 import logo from './logo.svg'
+
+if (window.location.hostname === 'badabu.netlify.com') {
+  ReactGA.initialize('UA-38195480-2')
+}
+console.log(window.location.hostname, window.location.hostname === 'badabu.netlify.com')
+
+ReactGA.pageview('/')
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import ReactGA from 'react-ga'
 import '@atlaskit/css-reset'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-
-// if (window.location.hostname === 'badabu.netlify.com') {
-ReactGA.initialize('UA-38195480-2')
-// }
-console.log('=====', window.location.hostname, window.location.hostname === 'badabu.netlify.com')
-
-ReactGA.pageview('/')
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
